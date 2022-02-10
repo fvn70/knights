00db6d8b4b16e294c08429934f5983b01a47830c
fvn70 <fvn_reg@mail.ru>
2022-02-10 09:07:48 +0700

    new Stage 1/6

---
+++ Knight's Tour Puzzle/task/game.py
@@ -0,0 +1,43 @@
+import re
+
+
+def draw_board():
+    cell_size = len(str(xmax * ymax))
+    lpad = len(str(ymax))
+    w = xmax * cell_size + 3
+    head = ' ' * lpad + '-' * w
+    bottom = [str(i+1) for i in range(xmax)]
+    print(head)
+    for r in range(ymax):
+        row = f"{ymax - r}|".rjust(lpad + 1)
+        print(row, end="")
+        for c in range(xmax):
+            print(f" {board[r][c]}", end="")
+        print(" |")
+    print(head)
+    print('   ' + ' '.join(bottom))
+
+def read_pos(msg):
+    is_move = 'move' in msg
+    while True:
+        cmd = input(msg)
+        if cmd and re.match(rexp, cmd) is not None:
+            x, y = map(int, cmd.split())
+            if x in range(1, xmax + 1) and y in range(1, ymax + 1) \
+                    and (x, y) != (x0, y0):
+                if not is_move or abs(x - x0) + abs(y - y0) == 3:
+                    break
+        if is_move:
+            print("Invalid move! ", end="")
+        else:
+            print("Invalid position!")
+    return x, y
+
+
+rexp = r"[1-9][0-9]? [1-9][0-9]?\Z"
+xmax, ymax = 8, 8
+board = [['_' for c in range(xmax)] for r in range(ymax)]
+x0, y0 = 0, 0
+x0, y0 = read_pos("Enter the knight's starting position: ")
+board[ymax - y0][x0 - 1] = 'X'
+draw_board()
