520d847b63dce0ca780d0316defc534fc878611a
fvn70 <fvn_reg@mail.ru>
2022-02-10 10:44:00 +0700

    new Stage 3/6

--- Knight's Tour Puzzle/task/game.py
+++ Knight's Tour Puzzle/task/game.py
@@ -9,6 +9,7 @@ def draw_board():
     cell = f" {'_' * cell_size}"
     bottom = [str(i+1).rjust(cell_size) for i in range(xmax)]
     knight = ' ' * cell_size + 'X'
+    move = ' ' * cell_size + 'O'
     print(head)
     for r in range(ymax):
         row = f"{ymax - r}|".rjust(lpad + 1)
@@ -16,12 +17,22 @@ def draw_board():
         for c in range(xmax):
             if board[r][c] == 'X':
                 row = knight
+            elif board[r][c] == 'O':
+                row = move
             else:
                 row = cell
             print(row, end="")
         print(" |")
     print(head)
     print(' ' * (lpad + 2) + ' '.join(bottom))
+
+
+def find_moves():
+    for move in moves:
+        x = x0 + move[0]
+        y = y0 + move[1]
+        if x in range(1, xmax + 1) and y in range(1, ymax + 1):
+            board[ymax - y][x - 1] = 'O'
 
 
 def read_dim():
@@ -50,10 +61,13 @@ def read_pos(msg):
     return x, y
 
 
+moves = [(-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1)]
 rexp = r"[1-9][0-9]? [1-9][0-9]?\Z"
 xmax, ymax = read_dim()
 board = [['_' for c in range(xmax)] for r in range(ymax)]
 x0, y0 = 0, 0
 x0, y0 = read_pos("Enter the knight's starting position: ")
 board[ymax - y0][x0 - 1] = 'X'
+find_moves()
+print("\nHere are the possible moves:")
 draw_board()
